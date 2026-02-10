f41882eee55db34f8c3cacc81615b2e787b15af6
rekorou <realchardy@gmail.com>
2026-02-10 17:34:24 +0800

    feat: logo

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,7 +1,11 @@
+import logo from '../assets/logo.png'
+
 export function AppHeader() {
   return (
     <header className="app-header">
-      <div className="app-logo">LOGO</div>
+      <div className="app-logo">
+        <img src={logo} alt="Quiz Bowl logo" style={{ height: '52px' }} />
+      </div>
       <div className="app-title">QUIZ BOWL</div>
       <div className="app-header-spacer" />
     </header>
